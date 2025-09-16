117feb5366d9755ac46ef4fdc13b78cdf4f1e368
richss108-cell <richss.108@gmail.com>
2025-09-16 01:28:37 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,445 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
 #include <stdio.h>
 #include <string.h>
-#include "filesystem.h"
+#include <stdbool.h>
+#include <math.h>
 
-int main() {
-    FileSystem *fs = fs_create("battlefs.dat");
-    char command[256], arg[256];
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+volatile int32_t pulse_count = 0;     // Pulsos del encoder
+int current_floor = 0;                // Piso actual
+int target_floor = 0;                 // Piso destino
 
-    while (1) {
-        printf("battlefs> ");
-        if (scanf("%s", command) != 1) break;
+/* USER CODE END Includes */
 
-        if (strcmp(command, "add") == 0) {
-            scanf("%s", arg);
-            fs_add_file(fs, arg);
-        } else if (strcmp(command, "get") == 0) {
-            scanf("%s", arg);
-            fs_get_file(fs, arg);
-        } else if (strcmp(command, "list") == 0) {
-            fs_list_files(fs);
-        } else if (strcmp(command, "exit") == 0) {
-            break;
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+// Carrete diámetro posible a tener en cuenta
+#define MM_PER_PULSE 2.866f
+#define PULSES_PER_CM 3.5f
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+// IR decoding
+TIM_HandleTypeDef htim2;   // Timer para medir tiempos IR
+TIM_HandleTypeDef htim3;   // Timer PWM motor
+volatile uint32_t ir_last = 0;
+volatile uint32_t ir_code = 0;
+volatile uint8_t ir_bitcount = 0;
+volatile bool ir_frame_received = false;
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+
+void motorSpeed(int16_t voltaje);
+float pulsesToMm(int32_t pulses);
+int32_t mmToPulses(float mm);
+void goToFloor(int floor);
+void processIR(uint32_t code);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+// ---------------------- Encoder (sensores A y B) ----------------------
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if(GPIO_Pin == GPIO_PIN_1) { // A
+        if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2) == GPIO_PIN_SET) {
+            pulse_count++;
         } else {
-            printf("Comando no reconocido.\n");
+            pulse_count--;
         }
     }
+    else if(GPIO_Pin == GPIO_PIN_2) { // B
+        if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) == GPIO_PIN_SET) {
+            pulse_count--;
+        } else {
+            pulse_count++;
+        }
+    }
+    else if(GPIO_Pin == GPIO_PIN_0) { // IR receiver
+        uint32_t now = __HAL_TIM_GET_COUNTER(&htim2);
+        uint32_t diff = (now >= ir_last) ? (now - ir_last) : (0xFFFFFFFF - ir_last + now);
+        ir_last = now;
 
-    fs_close(fs);
-    return 0;
+        if(diff > 10000) { // Nuevo frame
+            ir_code = 0;
+            ir_bitcount = 0;
+        } else if(diff > 1000 && diff < 2000) { // bit 0
+            ir_code <<= 1;
+            ir_bitcount++;
+        } else if(diff > 2000 && diff < 3000) { // bit 1
+            ir_code = (ir_code << 1) | 1;
+            ir_bitcount++;
+        }
+
+        if(ir_bitcount >= 32) {
+            ir_frame_received = true;
+            ir_bitcount = 0;
+        }
+    }
 }
+
+// ---------------------- Motor control ----------------------
+void motorSpeed(int16_t voltaje) {
+    uint16_t duty = (uint16_t)(abs(voltaje) * 99 / 4095); // map -4095..4095 a 0..99
+
+    if(voltaje > 0) {
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, duty);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 0);
+    } else if(voltaje < 0) {
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, 0);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, duty);
+    } else {
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, 0);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 0);
+    }
+}
+
+// ---------------------- Conversión Pulsos - Distancia ----------------------
+float pulsesToMm(int32_t pulses) {
+    return pulses * MM_PER_PULSE;
+}
+
+int32_t mmToPulses(float mm) {
+    return (int32_t)(mm / MM_PER_PULSE);
+}
+
+// ---------------------- Movimiento entre pisos ----------------------
+void goToFloor(int floor) {
+    if(floor < 0 || floor > 9) return;
+
+    target_floor = floor;
+    int diff = target_floor - current_floor;
+    int pulses_to_move = fabs(diff) * PULSES_PER_CM;
+
+    int32_t start_count = pulse_count;
+    if(diff > 0) {
+        motorSpeed(3000); // subir
+        while((pulse_count - start_count) < pulses_to_move);
+    } else if(diff < 0) {
+        motorSpeed(-3000); // bajar
+        while((start_count - pulse_count) < pulses_to_move);
+    }
+    motorSpeed(0);
+
+    current_floor = target_floor;
+
+    char msg[50];
+    sprintf(msg, "Piso actual: %d\r\n", current_floor);
+    CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
+}
+
+// ---------------------- Procesar códigos IR ----------------------
+void processIR(uint32_t code) {
+    // tabla de equivalencias
+    switch(code & 0xFF) {  // último byte del código NEC
+        case 0x16: goToFloor(0); break;
+        case 0x0C: goToFloor(1); break;
+        case 0x18: goToFloor(2); break;
+        case 0x5E: goToFloor(3); break;
+        case 0x08: goToFloor(4); break;
+        case 0x1C: goToFloor(5); break;
+        case 0x5A: goToFloor(6); break;
+        case 0x42: goToFloor(7); break;
+        case 0x52: goToFloor(8); break;
+        case 0x4A: goToFloor(9); break;
+        default: break;
+    }
+}
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  MX_USB_DEVICE_Init();
+
+  HAL_TIM_Base_Start(&htim2); // para medir tiempos IR
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
+  char msg[50];
+  sprintf(msg, "Sistema Ascensor Iniciado\r\n");
+  CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
+
+
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      if(ir_frame_received) {
+          processIR(ir_code);
+          ir_frame_received = false;
+      }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+// ---------------------- TIM3: PWM para motor ----------------------
+
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+    TIM_OC_InitTypeDef sConfigOC = {0};
+
+    htim3.Instance = TIM3;
+    htim3.Init.Prescaler = 31;
+    htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim3.Init.Period = 99;
+    htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if (HAL_TIM_PWM_Init(&htim3) != HAL_OK) {
+        Error_Handler();
+    }
+
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 0;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+
+    if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK) {
+        Error_Handler();
+    }
+    if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
+        Error_Handler();
+    }
+
+    HAL_TIM_MspPostInit(&htim3);
+
+  /* USER CODE END TIM3_Init 0 */
+
+
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // PA1 Sensor A
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // PA2 Sensor B
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // PA0 IR receiver
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // habilitar interrupciones
+    HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+    HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+    HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+
+  /*Configure GPIO pins : PA0 PA1 PA2 */
+
+
+  /* EXTI interrupt init*/
+
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
